8353a08357bbadbf202f293da6ced86ccc2e9fb7
Mir Aaqib Bashir <116267216+Aaqibmir123@users.noreply.github.com>
2023-01-26 22:25:27 +0530

    Add files via upload

---
+++ LOgout.js
@@ -0,0 +1,34 @@
+import { LoginContext } from "./LoginContext"
+import { useContext } from "react"
+import { useHistory } from "react-router-dom"
+
+import { Container, Card, Button, CardImg } from "react-bootstrap"
+
+const Logout = () => {
+    const ctx = useContext(LoginContext)
+    const history = useHistory();
+
+    const remove = () => {
+        ctx.logout();
+        history.replace('/Home/home.js');
+        ctx.items=[];
+     ctx.totalAmount = 0;
+    }
+    return (<><Container style={{ textAlign: "center", margin: "20px 100px 100px 200px" }} >
+
+
+        <Card.Body>
+            <Card.Text className="m-4"><h1 style={{ color: "Red" }} > Log-Out </h1></Card.Text>
+            <p>Click Below</p>
+            <Card.Text> ⬇️  </Card.Text>
+
+
+            <Button className="p-4" variant="danger" onClick={remove}>Logout
+            </Button>
+        </Card.Body>
+
+
+    </Container>
+    </>)
+}
+export default Logout;
